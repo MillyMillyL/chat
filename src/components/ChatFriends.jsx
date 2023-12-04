600a7d320a8e59b07e6ef1b3368cb3cc50c18919
Milly <liuhongmein@gmail.com>
2023-12-03 19:59:33 -0500

    chat friend tabs and setchatfriend

--- src/components/ChatFriends.jsx
+++ src/components/ChatFriends.jsx
@@ -1,22 +1,52 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { IoChatbubbleEllipsesOutline } from 'react-icons/io5';
 
 function ChatFriends() {
   const { userFriends } = useContext(AuthContext);
-  console.log(userFriends);
+  const [activeTab, setActiveTab] = useState('friendsTab');
+  const [chatFriend, setChatFriend] = useState(null);
 
   return (
     <div>
-      <div className="flex justify-between">
-        <button>Friend List</button>
-        <button>Friend Request</button>
+      <div className="flex justify-around">
+        <button
+          className={`${
+            activeTab === 'friendsTab' ? 'bg-green-500 text-white' : ''
+          }`}
+          onClick={() => setActiveTab('friendsTab')}
+        >
+          Friend List
+        </button>
+        <button
+          className={`${
+            activeTab === 'friendRequestsTab' ? 'bg-green-500 text-white' : ''
+          }`}
+          onClick={() => setActiveTab('friendRequestsTab')}
+        >
+          Friend Request
+        </button>
       </div>
-      <div>
-        {userFriends !== null &&
-          userFriends.map((friend) => (
-            <p key={friend.friendUserId}>{friend.friendName}</p>
-          ))}
-      </div>
+      {activeTab === 'friendsTab' && (
+        <ul className="p-4 ">
+          {userFriends !== null &&
+            userFriends.map((friend) => (
+              <li
+                key={friend.friendUserId}
+                className={`cursor-pointer p-1 ${
+                  chatFriend === friend.friendUserId ? 'font-bold' : ''
+                }`}
+                onClick={() => setChatFriend(friend.friendUserId)}
+              >
+                {chatFriend === friend.friendUserId && (
+                  <IoChatbubbleEllipsesOutline className="inline mr-2" />
+                )}
+                {friend.friendName}
+              </li>
+            ))}
+        </ul>
+      )}
+      {activeTab === 'friendRequestsTab' && <div>Friend requests</div>}
     </div>
   );
 }
